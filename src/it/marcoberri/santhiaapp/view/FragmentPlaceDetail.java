69bb9fc1733ce4422b7775d9a9350206f750e888
marcoberri <marcoberri@gmail.com>
2014-02-26 18:03:22 +0100

    add google maps

--- src/it/marcoberri/santhiaapp/view/FragmentPlaceDetail.java
+++ src/it/marcoberri/santhiaapp/view/FragmentPlaceDetail.java
@@ -1,5 +1,12 @@
 package it.marcoberri.santhiaapp.view;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import it.marcoberri.santhiaapp.R;
 import it.marcoberri.santhiaapp.model.PlaceListModel;
@@ -17,42 +24,58 @@ public class FragmentPlaceDetail extends Fragment {
 
 	protected final static String TAG = FragmentPlaceDetail.class.getName();
 
-	
 	private int position;
 	private PlaceListModel placeListModel;
-    
+	static final LatLng CHIESA = new LatLng(45.366255, 8.174720);
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
-		Log.i(TAG, "onCreateView()");
-		final View v = inflater.inflate(R.layout.fragment_place_detail, container,
-				false);
 
-		final TextView title = (TextView)v.findViewById(R.id.place_detail_title);
+		Log.i(TAG, "onCreateView()");
+		final View v = inflater.inflate(R.layout.fragment_place_detail,
+				container, false);
+
+		final TextView title = (TextView) v
+				.findViewById(R.id.place_detail_title);
 		title.setText(placeListModel.getTitle());
-		
+
 		final TabHost mTabHost = (TabHost) v.findViewById(android.R.id.tabhost);
 		setupTabs(mTabHost);
-		
-		
 
-/*		this..onKeyDown(int keyCode, KeyEvent event) {
-		    switch(keyCode){
-		    case KeyEvent.KEYCODE_BACK:
-		        // do something here 
-		        return true;
-		    }
-		    return super.onKeyDown(keyCode, event);
+		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(
+				R.id.tab_3_map)).getMap();
+
+		if (map != null) {
+
+			Marker kiel = map.addMarker(new MarkerOptions()
+					.position(CHIESA)
+					.title("Chiesa di Sant'Agata")
+					.snippet("Kiel is cool")
+					.icon(BitmapDescriptorFactory
+							.fromResource(R.drawable.ic_launcher)
+
+					)
+
+			);
+
+			kiel.setVisible(true);
+
+			map.moveCamera(CameraUpdateFactory.newLatLngZoom(CHIESA, 30));
+
 		}
-	*/	
-		
+
+		/*
+		 * this..onKeyDown(int keyCode, KeyEvent event) { switch(keyCode){ case
+		 * KeyEvent.KEYCODE_BACK: // do something here return true; } return
+		 * super.onKeyDown(keyCode, event); }
+		 */
+
 		return v;
 	}
 
-	
 	private void setupTabs(TabHost mTabHost) {
 
-		mTabHost.setup(); 
+		mTabHost.setup();
 		final TabSpec tabSpec1 = mTabHost.newTabSpec("Story");
 		tabSpec1.setContent(R.id.tab_1);
 		tabSpec1.setIndicator("Storia");
@@ -60,21 +83,21 @@ public class FragmentPlaceDetail extends Fragment {
 		final TabSpec tabSpec2 = mTabHost.newTabSpec("Near");
 		tabSpec2.setContent(R.id.tab_2);
 		tabSpec2.setIndicator("Foto");
-		
+
 		final TabSpec tabSpec3 = mTabHost.newTabSpec("Map");
 		tabSpec3.setContent(R.id.tab_3);
 		tabSpec3.setIndicator("Map");
 
-
-	    
 		mTabHost.addTab(tabSpec1);
 		mTabHost.addTab(tabSpec2);
 		mTabHost.addTab(tabSpec3);
-		
 
-		mTabHost.getTabWidget().getChildAt(0).getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.tab_height);
-		mTabHost.getTabWidget().getChildAt(1).getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.tab_height);
-		mTabHost.getTabWidget().getChildAt(2).getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.tab_height);
+		mTabHost.getTabWidget().getChildAt(0).getLayoutParams().height = getResources()
+				.getDimensionPixelSize(R.dimen.tab_height);
+		mTabHost.getTabWidget().getChildAt(1).getLayoutParams().height = getResources()
+				.getDimensionPixelSize(R.dimen.tab_height);
+		mTabHost.getTabWidget().getChildAt(2).getLayoutParams().height = getResources()
+				.getDimensionPixelSize(R.dimen.tab_height);
 
 	}
 
@@ -87,8 +110,8 @@ public class FragmentPlaceDetail extends Fragment {
 	}
 
 	public void setPlaceModel(PlaceListModel placeListModel) {
-		this.placeListModel=placeListModel;
-		
+		this.placeListModel = placeListModel;
+
 	}
 
 }
